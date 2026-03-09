70ad71283e8cf91ff9a9a683b71f75c22234a371
Rodri10a <rodrigojosearguello@gmail.com>
2026-03-09 14:41:55 -0300

    Agregar rutas de links y conectar router en app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const routes = require('./src/routes/routes')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'src/views'))
@@ -9,6 +10,8 @@ app.set('views', path.join(__dirname, 'src/views'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'src/public')))
+
+app.use('/', routes)
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000')
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 const {
   getAllTopics, getNewTopicForm, createTopic, showTopic,
-  getEditTopicForm, updateTopic, deleteTopic, voteTopic
+  getEditTopicForm, updateTopic, deleteTopic, voteTopic,
+  createLink, updateLink, deleteLink, voteLink
 } = require('../controllers/controller')
 
 // Topics
@@ -15,5 +16,11 @@ router.get('/topics/:id/edit', getEditTopicForm)
 router.post('/topics/:id/update', updateTopic)
 router.post('/topics/:id/delete', deleteTopic)
 router.post('/topics/:id/vote', voteTopic)
+
+// Links
+router.post('/topics/:id/links', createLink)
+router.post('/topics/:id/links/:linkId/update', updateLink)
+router.post('/topics/:id/links/:linkId/delete', deleteLink)
+router.post('/topics/:id/links/:linkId/vote', voteLink)
 
 module.exports = router

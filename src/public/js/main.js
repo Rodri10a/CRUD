936fa0a477ede84812e50e18f1c8c9e7112ad2c6
Rodri10a <rodrigojosearguello@gmail.com>
2026-03-09 20:25:48 -0300

    Simplificar toggleForm usando classList.toggle

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,5 @@
 function toggleForm(id) {
-  const el = document.getElementById(id)
-  el.style.display = el.style.display === 'none' ? 'block' : 'none'
+  document.getElementById(id).classList.toggle('hidden')
 }
 
 document.addEventListener('DOMContentLoaded', () => {

f1c5bf7c747c4f9158e4bb8c94dae6db87dcb4bb
Rodri10a <rodrigojosearguello@gmail.com>
2026-03-09 14:38:42 -0300

    Agregar middlewares para JSON, formularios y archivos estaticos

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'src/views'))
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'src/public')))
+
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000')
 })

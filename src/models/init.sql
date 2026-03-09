ce4c93b8d307b664a5017ec5fda871aebc8c6133
Rodri10a <rodrigojosearguello@gmail.com>
2026-03-09 14:39:05 -0300

    Agregar datos iniciales de ejemplo en init.sql

--- src/models/init.sql
+++ src/models/init.sql
@@ -16,3 +16,13 @@ CREATE TABLE links (
   url TEXT NOT NULL,
   votes INTEGER DEFAULT 0
 );
+
+INSERT INTO topics (title, description, votes) VALUES
+  ('JavaScript', 'Lenguaje de programacion de la web', 7),
+  ('Node.js', 'JavaScript del lado del servidor', 4),
+  ('Python', 'Lenguaje versatil para backend y data science', 1);
+
+INSERT INTO links (topic_id, title, url, votes) VALUES
+  (1, 'MDN Web Docs', 'https://developer.mozilla.org', 3),
+  (1, 'JavaScript.info', 'https://javascript.info', 5),
+  (2, 'Documentacion oficial', 'https://nodejs.org/docs', 2);
